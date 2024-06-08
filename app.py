51f357757e1898bea64e8f8c2d3cd07a92acc41f
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2024-06-08 13:42:01 +0700

    add import tensorflow

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import numpy as np
 import cv2
 import os
 import subprocess
+import tensorflow
 from collections import deque
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 
 model = load_model('Mobilenet_model.h5')
 
